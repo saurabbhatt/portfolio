f55e64ca26d8ae81361940f70557bb6cd78d6141
Saurabh Bhatnagar <saurabh.bhatnagar01@sap.com>
2025-07-07 20:14:14 +0530

    Fix GitHub Pages routing: switch to HashRouter and add homepage URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './shared/ui/styles/global-styles'
 import { Layout } from './widgets/layout'
